57320f85eb5e65f84f71cf6326aeca6ef92569c1
Nolan Byrnes <nolanehs@gmail.com>
2018-03-07 10:42:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
-
+accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
   def tags_attributes=(tag_hash)
      tag_hash.each do |i, tag_attributes|
        if tag_attributes[:name].present?
